501f63125ab20c5b0d8458dee6f00b9023442d98
Tshepiso-coder <lesegograce5@gmail.com>
2025-11-06 22:09:54 +0200

    Add files via upload

---
+++ Chapter09/2_car.py
@@ -0,0 +1,51 @@
+class Car:
+    """A simple attempt to represent a car."""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes to describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    
+    def read_odometer(self):
+        """Print a statement showing the car's mileage."""
+        print(f"\nThis car has {self.odometer_reading} miles on it.")
+
+    # MODIFY ATTRIBUTE VALUE THROUGH A METHOD
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
+# INSTANTIATE CAR OBJECT
+my_new_car = Car('audi', 'a4', 2024)
+print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
+
+# MODIFYING ATTRIBUTES DIRECTLY
+my_new_car.odometer_reading = 23
+my_new_car.read_odometer()
+
+# MODIFY ATTRIBUTE VALUE THROUGH A METHOD
+my_new_car.update_odometer(10)
+my_new_car.read_odometer()
+
+# INCREAMENT ATTRIBUTE VALUE TRHOUGH METHOD
+print("\nIncrement Funtion")
+my_new_car.increment_odometer(100)
+my_new_car.read_odometer()
